41af8afb377bdd1bbb9069b87c2f859d9ca749fc
Jagad R R <jagadraya388@gmail.com>
2024-10-25 14:34:30 +0700

    installed ready player me

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,5 +1,26 @@
-import { Text } from "@chakra-ui/react";
+import { AvatarCreator } from '@readyplayerme/react-avatar-creator';
+import { Avatar } from "@readyplayerme/visage";
+import { useState } from "react";
 
-export default function Index() {
-  return <Text fontSize={"4xl"}>Index</Text>;
+const config = {
+  clearCache: true,
+  bodyType: 'fullbody',
+  quickStart: false,
+  language: 'en',
+};
+
+const style = { width: '100%', height: '100vh', border: 'none' };
+
+export default function App() {
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const handleOnAvatarExported = (event) => {
+    setAvatarUrl(event.data.url);
+  };
+
+  return (
+    <>
+      <AvatarCreator subdomain="demo" config={config} style={style} onAvatarExported={handleOnAvatarExported} />
+      {avatarUrl && <Avatar modelSrc={avatarUrl} />}
+    </>
+  );
 }
