e8ad1f405eaea9336117f4bca2bd33d0249579cf
DANU DWIKI LAKSANA <ddlgamers14@gmail.com>
2024-10-26 15:14:56 +0700

    database seeder for instansi, nakes and user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Instansi;
+use App\Models\Nakes;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,95 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $instansiKesehatan = [
+            [
+                "name" => "RSUP Dr. Sardjito",
+                "address" => "Jl. Kesehatan No.1, Sekip, Sinduadi, Mlati, Sleman, Yogyakarta 55284"
+            ],
+            [
+                "name" => "RS Bethesda Yogyakarta",
+                "address" => "Jl. Jend. Sudirman No.70, Cokrodiningratan, Jetis, Yogyakarta 55224"
+            ],
+            [
+                "name" => "RS Panti Rapih",
+                "address" => "Jl. Cik Di Tiro No.30, Terban, Gondokusuman, Yogyakarta 55223"
+            ],
+            [
+                "name" => "RS PKU Muhammadiyah Yogyakarta",
+                "address" => "Jl. KH. Ahmad Dahlan No.20, Notoprajan, Ngampilan, Yogyakarta 55262"
+            ],
+            [
+                "name" => "RSUD Kota Yogyakarta",
+                "address" => "Jl. Ki Ageng Pemanahan No.1, Warungboto, Umbulharjo, Yogyakarta 55163"
+            ],
+            [
+                "name" => "RS Ludira Husada Tama",
+                "address" => "Jl. Wiratama No.5, Tegalrejo, Yogyakarta 55244"
+            ],
+            [
+                "name" => "RS Queen Latifa Yogyakarta",
+                "address" => "Jl. Wates Km. 3,5 No.99, Gamping, Sleman, Yogyakarta 55293"
+            ],
+            [
+                "name" => "RS Happy Land Medical Centre",
+                "address" => "Jl. Ipda Tut Harsono No.6, Muja Muju, Umbulharjo, Yogyakarta 55165"
+            ],
+            [
+                "name" => "RS Bhayangkara Yogyakarta",
+                "address" => "Jl. Raya Solo - Yogyakarta No.16, Kalasan, Sleman, Yogyakarta 55571"
+            ],
+            [
+                "name" => "RS Pratama Kota Yogyakarta",
+                "address" => "Jl. Gondosuli, Muja Muju, Umbulharjo, Yogyakarta 55165"
+            ]
+        ];
+
+        $nakes = [
+            [
+                "name" => "Naufal Hady AJ",
+                "username" => "naufaljr",
+                "password" => bcrypt("password"),
+                "address" => "Jl. Kesehatan No.1, Sekip, Sinduadi, Mlati, Sleman, Yogyakarta 55284",
+                "phone" => "081234567890",
+                "birth_date" => "1990-01-01",
+                "gender" => "pria",
+                "role" => "nakes"
+            ],
+            [
+                "name" => "Rizky Kurniawan",
+                "username" => "rizkykurniawan",
+                "password" => bcrypt("password"),
+                "address" => "Jl. Kesehatan No.1, Sekip, Sinduadi, Mlati, Sleman, Yogyakarta 55284",
+                "phone" => "081234567890",
+                "birth_date" => "1990-01-01",
+                "gender" => "pria",
+                "role" => "nakes"
+            ],
+        ];
+
+        foreach ($instansiKesehatan as $instansi) {
+            Instansi::create($instansi);
+        }
+
+        foreach ($nakes as $u) {
+            $user = User::create($u);
+            Nakes::create([
+                "user_id" => $user->id,
+                "instansi_id" => 1
+            ]);
+        }
+
+
+        User::create([
+            "name" => "Rafino Ramdhaniar PP",
+            "username" => "rafino",
+            "password" => bcrypt("password"),
+            "address" => "Jl. Kesehatan No.1, Sekip, Sinduadi, Mlati, Sleman, Yogyakarta 55284",
+            "phone" => "081234567890",
+            "birth_date" => "1990-01-01",
+            "gender" => "pria",
+            "role" => "pasien",
+            "bpjs_number" => fake()->unique()->numerify('############')
         ]);
     }
 }
